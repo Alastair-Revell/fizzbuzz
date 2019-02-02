72094721a30cb78a8cab437fc374b43a1eeb21c5
Alastair Revell <ar15153@outlook.com>
2019-02-02 18:51:26 +0000

    fizzbuzz

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(n)
+  if n % 5 == 0 && n % 3 == 0
+    'fizzbuzz'
+  elsif n % 3 == 0
+    'fizz'
+  elsif n % 5 == 0
+    'buzz'
+  else
+     n
+  end
+end
+
+(1..100).map {|n| fizzbuzz(n)}
